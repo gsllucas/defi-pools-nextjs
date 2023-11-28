f0d823ef9dd46ff263ca3550b8636b32dcaf367c
gsllucas <llucasgs@gmail.com>
2023-11-28 19:17:41 -0300

    :tada: Start project and configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { Header } from './components/Header/Header';
+import { ComponentResizing } from './layout/ComponentResizing';
+import { ToopPools } from './components/TopPools/TopPools';
+import { DrawerProvider } from './contexts/DrawerContext';
+import { AppDrawer } from './components/AppDrawer/AppDrawer';
+import { AppContent } from './components/AppContent/AppContent';
 
 export default function Home() {
+  // async function getPools() {
+  //   try {
+  //     const queryParams: QueryParams = {
+  //       chain: 'Polygon',
+  //       exchange: 'uniswap-v3',
+  //       orderBy: 'volumeTvl',
+  //       orientation: 'desc',
+  //     };
+  //     const response = await fetch(
+  //       `${environment.API_URL}/pools?chain=${queryParams.chain}&exchange=${queryParams.exchange}&orderBy=${queryParams.orderBy}&orientation=${queryParams.orientation}`
+  //     );
+  //     const jsonResponse = await response.json();
+  //     setPools(jsonResponse.pools);
+  //     return () => {};
+  //   } catch (err) {
+  //     setPools([]);
+  //   }
+  // }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <DrawerProvider>
+        <ComponentResizing open={true}>
+          <Header />
+          <ToopPools />
+          <AppContent />
+        </ComponentResizing>
+        <AppDrawer />
+      </DrawerProvider>
     </main>
-  )
+  );
 }
